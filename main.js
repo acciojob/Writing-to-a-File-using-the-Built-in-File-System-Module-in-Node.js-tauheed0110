a2dbc7331b87f2d92910e163a74ce6e402f80687
saif0110 <khanmohdtauheed4@gmail.com>
2024-12-31 17:11:06 +0530

    file changed

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 const fs = require('fs');
 
 // TODO: Write the message "Hello, World!" to the file "output.txt"
-
+fs.writeFile('./output.txt', 'Hello, World!', (err) => {
+    if(err){
+        throw new Error('Error occured while writing in file');
+        console.log(err);
+        return;
+    }
+    console.log('Written in the file successfully');
+})
